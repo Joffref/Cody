90d7b5dbe7b4f6b565b3abe799835665b6b4c64d
Mathis Joffre <51022808+Joffref@users.noreply.github.com>
2019-05-25 18:29:51 +0200

    Add files via upload

---
+++ Cody_main.js
@@ -0,0 +1,33 @@
+//import from own commands
+let Discord = require('discord.js');
+//My commands 
+let Python = require('./commands/python');
+let Web = require('./commands/web')
+let Ruby = require('./commands/ruby')
+let Kotlin = require('./commands/kotlin')
+//bot creation
+const client = new Discord.Client();
+
+//check start
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+//New Member message
+client.on('guildMemberAdd', member => {
+    
+    member.createDM().then( function (channel){
+        channel.send('Bienvenue à toi' + member.displayName)
+    });
+
+});
+
+//functions 'on message' from ./commands, can be improved..
+client.on('message', message => {
+   let command_used =  Python.parse(message) || Web.parse(message) || Ruby.parse(message) || Kotlin.parse(message)
+});
+  
+//login 
+client.login('NTM4MTA5NjEzNTE0NDI0MzQz.Dy-laA.O9FNlYhvNtZqruehpqUuNwz_9mI');
